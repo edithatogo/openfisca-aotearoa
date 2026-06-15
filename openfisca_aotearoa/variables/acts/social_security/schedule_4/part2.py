e6d33a69819936bfa6bda6b32c65b6ccbe61b54d
Dylan Mordaunt <Dylan.Mordaunt@health.nsw.gov.au>
2026-06-16 00:14:05 +1000

    feat: add social security schedule 4 part 2

---
+++ openfisca_aotearoa/variables/acts/social_security/schedule_4/part2.py
@@ -0,0 +1,23 @@
+"""This module provides eligibility rates for Sole Parent Support under Schedule 4, Part 2."""
+
+import numpy
+
+from openfisca_core import periods, variables
+
+from openfisca_aotearoa import entities
+
+
+class schedule_4__part2_1(variables.Variable):
+    value_type = bool
+    entity = entities.Person
+    definition_period = periods.WEEK
+    reference = "https://www.legislation.govt.nz/act/public/2018/0032/latest/DLM6784854.html"
+    label = "Part 2 Sole Parent Support - Clause 1 (single person with dependent children)"
+
+    def formula_2018_11_26(persons, period, parameters):
+        single = numpy.logical_not(persons("social_security__in_a_relationship", period))
+        has_dependent_children = persons("social_security__dependent_children", period) > 0
+        principal_caregiver = persons("social_security__principal_caregiver", period)
+        is_parent = persons.has_role(entities.Family.PARENT)
+
+        return single * has_dependent_children * (principal_caregiver + is_parent)
